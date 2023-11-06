ed2fefd8ef7f9e96c88eb11c4ac8d1a77b458ffa
Tommy Green <tommygreen@Tommys-Mac-mini.local>
2023-11-06 22:47:59 +0800

    add debug info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// use rustpython_parser::ast::visitor;
 use rustpython_parser::ast::Stmt;
 use rustpython_parser::text_size::TextRange;
 use rustpython_parser::{ast, Parse};
@@ -14,7 +15,18 @@ fn find_import_statements(code: &str) -> Vec<TextRange> {
             Stmt::ImportFrom(import_from_statement) => {
                 imports.push(import_from_statement.range);
             }
-            _ => {}
+            Stmt::Assign(assign_statement) => {
+                imports.push(assign_statement.range);
+                println!("assign value is {:?}", assign_statement.value)
+                //expr.value
+            }
+            Stmt::Expr(expr) => {
+                imports.push(expr.range);
+                println!("expr value is {:?}", expr.value)
+            }
+            _ => {
+                println!("Unhandled statement: {:?}", statement);
+            }
         }
     }
 
@@ -22,7 +34,7 @@ fn find_import_statements(code: &str) -> Vec<TextRange> {
 }
 
 fn main() {
-    let code = "from objects.asset import AssetTask, AssetAccountCheck, AssetCheck, AssetAccountScan, AssetScan, Asset, \\\n    AssetAccountScanRelation, Account, DEFAULT_VPC_NAME, Tag";
+    let code = "(1, 2,)\nt=((1, 2), 3)\nf((1, 1,), 2)\nfrom objects.asset import AssetTask, AssetAccountCheck, AssetCheck, AssetAccountScan, AssetScan, Asset, \\\n    AssetAccountScanRelation, Account, DEFAULT_VPC_NAME, Tag\nl = 1\nl += 1";
 
     let import_statements = find_import_statements(code);
     for statement in import_statements {
@@ -32,4 +44,7 @@ fn main() {
         let _end: usize = statement.end().into();
         println!("{}", &code[_start.._end])
     }
+
+    // let code1 = "lst = [1, 2, 3]";
+    // let python_expr = ast::Expr::parse(code1, "<test>").unwrap(); // or expr
 }
