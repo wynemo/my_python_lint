cdd81d0e4b20f8115d3174230bca953f0a72faa5
Tommy Green <tommygreen@Tommys-Mac-mini.local>
2023-11-12 23:34:29 +0800

    read folder or file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 use rustpython_parser::ast::Stmt;
 use rustpython_parser::text_size::TextRange;
 use rustpython_parser::{ast, Parse};
+use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 fn find_import_statements(code: &str) -> Vec<TextRange> {
     let ast = ast::Suite::parse(code, "<test>").unwrap();
@@ -35,15 +38,39 @@ fn find_import_statements(code: &str) -> Vec<TextRange> {
     imports
 }
 
-fn main() {
-    let mut file = File::open("test.py").expect("Failed to open file");
-
-    // Read the file contents into a string
+fn read_file_contents(file_path: &Path) -> Result<String, std::io::Error> {
+    let mut file = fs::File::open(file_path)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Failed to read file");
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
 
-    let code: &str = &contents;
+fn read_python_files<F>(folder_path: &Path, process_file: &mut F) -> Result<(), std::io::Error>
+where
+    F: FnMut(&Path, &str),
+{
+    let entries = fs::read_dir(folder_path)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let file_path = entry.path();
+
+        if file_path.is_file() && file_path.extension().map_or(false, |ext| ext == "py") {
+            if let Ok(contents) = read_file_contents(&file_path) {
+                process_file(&file_path, &contents);
+            }
+        } else if file_path.is_dir() {
+            if let Err(err) = read_python_files(&file_path, process_file) {
+                eprintln!("Error reading python files: {}", err);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_source(path: &Path, code: &str) {
+    println!("{:?}", path);
     let import_statements = find_import_statements(code);
     for statement in import_statements {
         println!("{:?}", statement.start());
@@ -52,6 +79,40 @@ fn main() {
         let _end: usize = statement.end().into();
         println!("{}", &code[_start.._end])
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run -- <path>");
+        return;
+    }
+
+    let path = Path::new(&args[1]);
+
+    if path.is_file() && path.extension().map_or(false, |ext| ext == "py") {
+        if let Ok(contents) = read_file_contents(&path) {
+            // println!("File: {}\nContents:\n{}\n", path.display(), contents);
+            parse_source(path, &contents)
+        }
+    } else if path.is_dir() {
+        let mut process_file = parse_source;
+        if let Err(err) = read_python_files(&path, &mut process_file) {
+            eprintln!("Error reading python files: {}", err);
+        }
+    } else {
+        eprintln!("Invalid path: {}", path.display());
+    }
+
+    // let mut file = File::open("test.py").expect("Failed to open file");
+
+    // // Read the file contents into a string
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)
+    //     .expect("Failed to read file");
+
+    // let code: &str = &contents;
 
     // let code1 = "lst = [1, 2, 3]";
     // let python_expr = ast::Expr::parse(code1, "<test>").unwrap(); // or expr
